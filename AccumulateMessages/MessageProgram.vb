dc5570e6ace2e49ec798f2cee301e511de956569
Christopher-isu <christopherzakrev@isu.edu>
2025-02-16 21:29:28 -0700

    updated code

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -1,4 +1,11 @@
+'ChristopherZ
+'Spring 2025
+'RCET2265
+'Accumulate Messages
+'https://github.com/Christopher-isu/AccumulateMessages.git
 
+Option Explicit On 'forces all variables to be declared
+Option Strict On   'forces all variables to be declared of the correct data type
 Imports System
 
 Module MessageProgram
@@ -9,8 +16,18 @@ Module MessageProgram
     End Sub
 
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
-        'your code here
-        Return messages
+        Static messages As String 'static variables retain their value between function calls
+
+        If clear Then 'clear the messages by isnerting an empty string
+            messages = ""
+        Else
+            If messages <> "" Then  'if messages is not empty, add a new line
+                messages = messages & Chr(13) & Chr(10) 'Chr(13) is a carriage return, Chr(10) is a line feed
+            End If
+            messages = messages & newMessage 'add the new message to the existing messages
+        End If
+
+        UserMessages = messages 'return the messages
     End Function
 
 
